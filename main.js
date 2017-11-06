2109a36e3fef0d5939f76a8416e06bf6968a82d7
ENGEBRETSON <engebrea@otc.edu>
2017-11-06 12:36:09 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,39 @@
+$(function () {
+
+    const vm = new Vue({
+        el: '#app',
+        data: {
+            loading: false,
+            showing: 'films',
+            films: [],
+            film: null,
+            people: []
+        },
+        methods: {
+            loadFilms() {
+                this.loading = true
+
+                this.$http.get('https://ghibliapi.herokuapp.com/films')
+                    .then(resp => {
+                        this.films = resp.body
+                        this.loading = false
+                    })
+            },
+            selectFilm(film) {
+                this.film = film
+                this.showing = 'people'
+                this.loadPeople()
+            },
+            loadPeople() {
+                this.people = []
+                this.film.people.forEach(person => {
+                    this.$http.get(person)
+                        .then(resp => {
+                            this.people.push(resp.body)
+                        })
+                })
+            }
+        }
+    })
+
+})
